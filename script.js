2c52356af40d2a29abb84734293f3f04bae76dc8
superjhay29 <105788845+superjhay29@users.noreply.github.com>
2024-02-07 23:54:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function showDownloadLink() {
     
     if (confirmDownload) {
         document.getElementById('downloadLink').style.display = 'block';
-        document.getElementById('downloadLink').innerHTML = 'Download tool now<a href="https://download1531.mediafire.com/mrcpb9fmx0ugf4RDkrCEgoomUa4tB3HmHiE8Fq5uE93yir4YhOdkN6D2ZpPaxKFfNwPQwW5XUeKVUHZxp8VdO6hfrm44wK6od1s9WAP5xUiq4bC0ap2q2reaOojQqx8rCtAfdUcsAUHFxZNFgHCjOKNBgoE7KbiKbB36saix17qv9A/w6342v3tgerxdba/XST+official.zip">here</a>.';
+        document.getElementById('downloadLink').innerHTML = 'Download tool now<a href="https://www.mediafire.com/file/w6342v3tgerxdba/XST_official.zip/file">here</a>.';
     }
 }
