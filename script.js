43f5435897c8e7c5c5d9303b81f19d37a8399a52
superjhay29 <105788845+superjhay29@users.noreply.github.com>
2024-04-30 22:33:14 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-  function showDownloadLink() {
-            var confirmDownload = confirm("DISABLE ANY ANTIVIRUS OR REAL TIME PROTECTION");
-            
-            if (confirmDownload) {
-                document.getElementById('downloadLink').style.display = 'block';
-                document.getElementById('downloadLink').innerHTML = 'Download tool now <a href="https://www.mediafire.com/folder/hfjcov4f3q8ww/Release">here</a>.';
-            }
-        }
